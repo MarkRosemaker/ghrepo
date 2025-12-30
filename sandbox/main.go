93c228c669e34df62089ce69e1d7c5cd773fd9b2
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2025-12-30 19:52:13 +0100

    auto commit

--- sandbox/main.go
+++ sandbox/main.go
@@ -9,14 +9,6 @@ import (
 	"github.com/MarkRosemaker/ghrepo"
 )
 
-var opts = []ghrepo.Option{
-	ghrepo.WithBaseDir("/Users/mark/go/src/github.com/"),
-	ghrepo.MakeDirAll,
-	ghrepo.InitGit,
-	ghrepo.CreateRemote,
-	ghrepo.CreateOnGitHub,
-}
-
 func main() {
 	if err := do(context.Background()); err != nil {
 		log.Fatal(err)
@@ -24,35 +16,28 @@ func main() {
 }
 
 func do(ctx context.Context) error {
-	s := ghrepo.NewService(ctx, os.Getenv("GITHUB_TOKEN"), opts...)
+	s := ghrepo.NewService(ctx, os.Getenv("GITHUB_TOKEN"),
+		ghrepo.WithBaseDir("/Users/mark/go/src/github.com/"),
+		ghrepo.MakeDirAll,
+		ghrepo.InitGit,
+		ghrepo.CreateRemote,
+		ghrepo.CreateOnGitHub,
+	)
 
 	r, err := s.NewRepository(ctx, "MarkRosemaker", "ghrepo")
 	if err != nil {
 		return err
 	}
 
-	// if err := r.CommitAll("auto commit"); err != nil {
-	// 	return err
-	// }
+	if err := r.CommitAll("auto commit"); err != nil {
+		return err
+	}
 
 	if err := r.Push(ctx); err != nil {
 		return err
 	}
 
-	return nil
-
-	r, err = s.NewRepository(ctx, "MarkRosemaker", "oauth2local", opts...)
-	if err != nil {
-		return err
-	}
-
 	fmt.Printf("r: %#v\n", r)
 
-	r, err = s.NewRepository(ctx, "MarkRosemaker", "foo", opts...)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("r: %#v\n", r)
 	return nil
 }
